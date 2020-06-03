047b4c561cd7153dd28df4b06c19d41e42f4fa53
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2020-06-03 08:58:09 +0800

    Setting up IOS debug build

---
+++ ios/build/Pods.build/Debug-iphonesimulator/path_provider.build/Objects-normal/x86_64/path_provider-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/johnhigginsm.avila/MyFiles/Straat/staat-info-v3/ios/Pods/Target\ Support\ Files/path_provider/path_provider-dummy.m \
+  /Users/johnhigginsm.avila/MyFiles/Straat/staat-info-v3/ios/build/Pods.build/Debug-iphonesimulator/path_provider.build/module.modulemap \
+  /Users/johnhigginsm.avila/MyFiles/Straat/staat-info-v3/ios/Pods/Target\ Support\ Files/path_provider/path_provider-prefix.pch
